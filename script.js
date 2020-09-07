fbe10066dcf941d6b8778632f3b2345382d29363
Donna Engelberger <dengelberger@gmail.com>
2020-09-07 10:44:05 -0400

    work in progress to make a time conversion tool for the class

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// function to convert the time entered
+
+// convert to military time, then subtract the start time and return the answer
+
+// function myFunction() {
+    var startTimeEl = document.getElementById("startTime");
+    // var eventTimeEL = document.getElementById("#eventTime");
+
+    // var videoTime = startTimeEl.value - eventTimeEl.value;
+
+    console.log (startTimeEl);
+    // console.log(eventTimeEl);
+// }
+// return the video time to the screen.
+
+
